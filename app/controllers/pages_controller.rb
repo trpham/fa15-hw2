e6aa71ed8181eede6ada5a62bc63ad1045922122
Truong Pham <phamcse@berkeley.edu>
2015-10-01 23:31:30 -0700

    hw2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,23 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
-    @text = "You are nothing!"
+    if params[:name].blank? and params[:adjective].blank?
+      @text = "You are nothing!"
+    else
+      @text = params[:name] + " is so " + params[:adjective]
   end
 
   def age
   end
 
   def person
+    p = person.new params[:name], params[:age]
+    @a = p.introduce
+    @b = p.birth_year
+    @c = p.nickname
   end
 end
